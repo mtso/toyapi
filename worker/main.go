c7e842f48c0157a37a840ff7d5d214b3ffe2d598
mtso <mtso@tuta.io>
2017-12-07 12:33:06 -0800

    Try sqs

---
+++ worker/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func main() {
+	sess := session.Must(session.NewSession())
+
+	svc := sqs.New(sess)
+
+	qUrl := os.Getenv("QURL")
+
+	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            &qUrl,
+		MaxNumberOfMessages: aws.Int64(2),
+		VisibilityTimeout:   aws.Int64(36000), // 10 hours
+		WaitTimeSeconds:     aws.Int64(0),
+	})
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	if len(result.Messages) == 0 {
+		fmt.Println("Received no messages")
+		return
+	}
+
+	for _, v := range result.Messages {
+		fmt.Println(v)
+	}
+}
